62b2150e02ba418e006e087dcd653b73e5ab2f22
Hristo <h.georgiew93@gmail.com>
2021-02-25 11:26:48 +0200

    Update app.js and connection to database

--- app.js
+++ app.js
@@ -5,11 +5,25 @@ const path = require('path');
 const app = express();
 
 
+//Configure Mongoose to Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/cms_site', {useNewUrlParser: true}).then(response => {
+    console.log('MongoDB Connected Succssesfully.');
+}).catch(err => {
+    console.log('Dtabase Connection failed');
+})
+
+
 //Configure express
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+//routes
+app.use('/', (req, res) => {
+    res.send('Welcome');
 })
+
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+});
