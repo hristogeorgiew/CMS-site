8ab2f830c1284dd45a3799aa6138c2be74270a34
Hristo <h.georgiew93@gmail.com>
2021-02-25 17:36:53 +0200

    Create model

---
+++ models/PostModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: String,
+        default: 'public'
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    CreationDate: {
+        type: Date,
+        default: Date.now()
+    }
+    
+});
